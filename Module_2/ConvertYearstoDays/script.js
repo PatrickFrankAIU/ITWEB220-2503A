e1e98e2b58a50adb01a7ca2fd2d50ba8f78dcb96
DaFunkMunk <86984288+DaFunkMunk@users.noreply.github.com>
2025-05-28 16:10:13 -0500

    Completed Years to Days

---
+++ Module_2/ConvertYearstoDays/script.js
@@ -0,0 +1,10 @@
+function calculateAgeInDays() {
+  // Ask for age in years
+  let ageInYears = prompt("Please enter your age in years:");
+
+  // Convert and calculate
+  let ageInDays = parseFloat(ageInYears) * 365;
+
+  // Output to console
+  console.log("You are approximately " + ageInDays + " days old.");
+}
